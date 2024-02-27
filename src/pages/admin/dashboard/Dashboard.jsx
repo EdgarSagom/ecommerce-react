b0dfcd38f0cb44da91fb2c2796439fc0d52e271e
EdgarSago <edgar13sago@gmail.com>
2024-02-26 23:54:19 -0600

    total on Dashboard

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -9,7 +9,8 @@ import { FaUserTie } from 'react-icons/fa'
 
 function Dashboard () {
   const context = useContext(myContext)
-  const { mode } = context
+  const { mode, product, order, user } = context
+  // console.log(product, order, user)
 
   return (
     <Layout>
@@ -22,52 +23,52 @@ function Dashboard () {
           <div className='flex flex-wrap -m-4 text-center'>
             <div className='p-4 md:w-1/3 sm:w-1/2 w-full'>
               <div
-                className=' border-2 hover:shadow-orange-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300 px-4 py-3 rounded-xl'
+                className=' border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300 px-4 py-3 rounded-xl'
                 style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}
               >
-                <div className='text-orange-600 w-12 h-12 mb-3 inline-block'>
+                <div className='text-purple-600 w-12 h-12 mb-3 inline-block'>
                   <FaUserTie size={50} />
                 </div>
                 <h2
                   className='title-font font-medium text-3xl text-black fonts1'
                   style={{ color: mode === 'dark' ? 'white' : '' }}
-                >10
+                >{product.length}
                 </h2>
-                <p className=' text-orange-600 font-bold'>Total Products</p>
+                <p className=' text-purple-600 font-bold'>Total Products</p>
               </div>
             </div>
 
             <div className='p-4 md:w-1/3 sm:w-1/2 w-full'>
               <div
-                className=' border-2 hover:shadow-orange-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300 px-4 py-3 rounded-xl'
+                className=' border-2 hover:shadow-pink-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300 px-4 py-3 rounded-xl'
                 style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}
               >
-                <div className='text-orange-600 w-12 h-12 mb-3 inline-block'>
+                <div className='text-pink-600 w-12 h-12 mb-3 inline-block'>
                   <FaUserTie size={50} />
                 </div>
                 <h2
                   className='title-font font-medium text-3xl text-black fonts1'
                   style={{ color: mode === 'dark' ? 'white' : '' }}
-                >10
+                >{order.length}
                 </h2>
-                <p className=' text-orange-600  font-bold'>Total Orders</p>
+                <p className=' text-pink-600  font-bold'>Total Orders</p>
               </div>
             </div>
 
             <div className='p-4 md:w-1/3 sm:w-1/2 w-full'>
               <div
-                className=' border-2 hover:shadow-orange-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300 px-4 py-3 rounded-xl'
+                className=' border-2 hover:shadow-green-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300 px-4 py-3 rounded-xl'
                 style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}
               >
-                <div className='text-orange-600 w-12 h-12 mb-3 inline-block'>
+                <div className='text-green-600 w-12 h-12 mb-3 inline-block'>
                   <FaUserTie size={50} />
                 </div>
                 <h2
                   className='title-font font-medium text-3xl text-black fonts1'
                   style={{ color: mode === 'dark' ? 'white' : '' }}
-                >20
+                >{user.length}
                 </h2>
-                <p className='text-orange-600  font-bold'>Total Users</p>
+                <p className='text-green-600  font-bold'>Total Users</p>
               </div>
             </div>
           </div>
